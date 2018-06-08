1ec9720806efa24d8606dd95b491a4e8c98d1e13
Michael <m.coughlan18@gmail.com>
2018-06-09 01:55:01 +0200

    Removed redundant code

--- client/src/app/components/profile/statistics/statistics.component.ts
+++ client/src/app/components/profile/statistics/statistics.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.css']
 })
-export class StatisticsComponent implements OnInit {
+export class StatisticsComponent {
   public moneyDonated: Number = 1750;
 
   public resourcesGathered: Number[] = [20, 30, 50];
@@ -14,8 +14,4 @@ export class StatisticsComponent implements OnInit {
   public volunteersTrained: Object[] = [ { data: [250], label: 'Volunteers Trained' }];
 
   public pandasSaved: Object[] = [ { data: [34], label: 'Pandas Saved' } ];
-
-  public constructor() { }
-
-  ngOnInit() {}
 }
