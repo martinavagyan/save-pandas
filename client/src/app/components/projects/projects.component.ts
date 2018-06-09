17b46f02ccad75b7d4e02c180fe5ef668b761732
martinavagyan <martinavagyan@gmail.com>
2018-06-09 10:47:29 +0200

    some titles and images

--- client/src/app/components/projects/projects.component.ts
+++ client/src/app/components/projects/projects.component.ts
@@ -9,53 +9,53 @@ export class ProjectsComponent implements OnInit {
   public isMapVisible: Boolean = true;
   public sampleProjects = [{
     "id": 1,
-    "title": "Sinbad: Legend of the Seven Seas",
+    "title": "Water initiative in saharan africa",
     "subtitle": "Antigen Laboratories, Inc.",
     "short_description": "Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue.",
     "full_description": "Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.",
-    "image_url": "http://dummyimage.com/400x300.png/5fa2dd/ffffff"
+    "image_url": "assets/twokids.jpg"
   }, {
     "id": 2,
-    "title": "Street Fighter",
+    "title": "Primary Education in Zimbabwe",
     "subtitle": "Sanofi-Aventis U.S. LLC",
     "short_description": "Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue.",
     "full_description": "Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem. Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.",
-    "image_url": "http://dummyimage.com/400x300.png/cc0000/ffffff"
+    "image_url": "assets/morekids.jpg"
   }, {
     "id": 3,
-    "title": "See Spot Run",
+    "title": "Secondary Education in Bangladesh",
     "subtitle": "AvPAK",
     "short_description": "Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue.",
     "full_description": "Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem. Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat.",
-    "image_url": "http://dummyimage.com/400x300.png/cc0000/ffffff"
+    "image_url": "assets/schoolkids.jpg"
   }, {
     "id": 4,
-    "title": "Blood of Heroes, The (Salute of the Jugger, The)",
+    "title": "Recycling in south asia",
     "subtitle": "Lundbeck LLC",
     "short_description": "Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue.",
     "full_description": "In hac habitasse platea dictumst. Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat. Curabitur gravida nisi at nibh.",
-    "image_url": "http://dummyimage.com/400x300.png/dddddd/000000"
+    "image_url": "assets/ecosystem.jpg"
   }, {
     "id": 5,
-    "title": "Project A 2",
+    "title": "Child labour east asia",
     "subtitle": "L. Perrigo Company",
     "short_description": "Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue.",
     "full_description": "Quisque ut erat. Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.",
-    "image_url": "http://dummyimage.com/400x300.png/ff4444/ffffff"
+    "image_url": "assets/lostfreedom.jpg"
   }, {
     "id": 6,
-    "title": "Stalingrad",
-    "subtitle": "Major Pharmaceuticals",
+    "title": "Amazon river annual flood",
+    "subtitle": "Side effects of global warming",
     "short_description": "Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue.",
     "full_description": "Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum. Proin eu mi.",
-    "image_url": "http://dummyimage.com/400x300.png/dddddd/000000"
+    "image_url": "assets/nature.jpg"
   }, {
     "id": 7,
-    "title": "Gods Must Be Crazy II, The",
+    "title": "Sustainable crops project",
     "subtitle": "Bausch & Lomb Incorporated",
     "short_description": "Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue.",
     "full_description": "Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; ",
-    "image_url": "http://dummyimage.com/400x300.png/dddddd/000000"
+    "image_url": "assets/crops.jpg"
   }];
 
   constructor() { }
