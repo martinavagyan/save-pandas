bb2f7c2bd7abb4fbbfa215bbe399716fa952b794
Albert Hristov <albert.hristov@nl.ibm.com>
2018-06-12 16:37:33 +0200

    this is it :)

--- client/src/app/components/projects/project-detail/project-detail.component.ts
+++ client/src/app/components/projects/project-detail/project-detail.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {TransactionService} from "../../../services/transaction.service";
-import {Transaction} from "../../../services/transaction";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TransactionService } from "../../../services/transaction.service";
+import { Transaction } from "../../../services/transaction";
+
 
 @Component({
   selector: 'app-project-detail',
@@ -21,12 +22,14 @@ export class ProjectDetailComponent implements OnInit {
   public isLoadingDone: boolean = false;
   public iotaUrl: string = "assets/iota.png"
 
-  public volunteersTrained: Object[] = [{data: [250], label: 'Volunteer training'}, { data: [275], label: 'Medicine' }, ];
-  public pandasSaved: Object[] = [ { data: [34], label: 'Field hospitals' }, { data: [250], label: 'L of water' }, { data: [40], label: 'Trucks with medicine' },  ];
+  public donationsRaised: number = 700032;
+  public totalPledge: number = 10000000;
+  public volunteersTrained: Object[] = [{ data: [250], label: 'Volunteer training' }, { data: [275], label: 'Medicine' },];
+  public pandasSaved: Object[] = [{ data: [34], label: 'Field hospitals' }, { data: [250], label: 'L of water' }, { data: [40], label: 'Trucks with medicine' },];
   public projectGoals: Array<Transaction> = null;
 
   public constructor(private route: ActivatedRoute,
-                     private transactionService: TransactionService) {
+    private transactionService: TransactionService) {
   }
 
   ngOnInit() {
@@ -34,12 +37,29 @@ export class ProjectDetailComponent implements OnInit {
     // this.transactionService.addTransaction(this.projectGoals[1]).subscribe();
     // this.transactionService.addTransaction(this.projectGoals[2]).subscribe();
     this.isLoadingDone = false;
-
+    this.donations();
     this.transactionService.getLatestNTransactions(5).subscribe((result) => {
       console.log(result);
       this.projectGoals = result;
       this.isLoadingDone = true;
     });
   }
+  public donate(amount) {
+    this.donationsRaised = amount + this.donationsRaised;
+    return this.donationsRaised;
+  }
 
+  public donations() {
+    // for (let index = 0; index < array.length; index++) {
+    //   const element = array[index];
+    setInterval (() => {
+      console.log("Hello from setInterval");
+      if (this.donationsRaised <= this.totalPledge) {
+          const min = Math.ceil(500);
+          const max = Math.floor(1000);
+          this.donationsRaised += Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+        console.log(this.donationsRaised);
+      }, 3500);
+  }
 }
