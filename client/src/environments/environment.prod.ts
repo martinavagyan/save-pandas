d216ec18fa2c1668c4e01b23a61ac2c47a177f5e
martinavagyan <martinavagyan@gmail.com>
2018-06-12 17:36:43 +0200

    build client

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,4 +2,7 @@ export const environment = {
   production: true,
   apiPrefix:  'api/',
   apiHost:    'https://mean-deploy.herokuapp.com/',
+  mapbox:     {
+    accessToken: 'pk.eyJ1IjoibWFydGluYXZhZ3lhbiIsImEiOiJjamZwZWdiMDAxbGVuMzNsOWtsa21waXN6In0.W09trbbm-h6pTb_JfQ_4jw',
+  }
 };
